c10def00aa9c2a59c1bff01537ad7a557953c791
AshidaShun <172578742+AshidaShun@users.noreply.github.com>
2025-11-24 22:17:51 +0900

    11.24

--- Notebook/01_paiza_learning_log.py
+++ Notebook/01_paiza_learning_log.py
@@ -1,3 +1,35 @@
 # %%
-print("Hello, paiza!")
+import traceback
+import sys
+# %%
+# 例外処理
+## 簡単な例外処理
+
+print(1)
+try:
+    number = 0
+    answer = 100 / number
+    print(answer)
+except ZeroDivisionError:
+    print("0では割り算できません")
+    # print(traceback.format_exc())
+    sys.stderr.write(traceback.format_exc())
+finally:
+    print(2)
+# %%
+print(1)
+try:
+    number = 0
+    answer = 100 / number
+    print(answer)
+except ZeroDivisionError as e:
+    print("0では割り算できません")
+    print(e)
+except NameError as e:
+    print("未定義の変数を呼び出しています")
+    print(e)
+except Exception:
+    print("予期せぬエラーが発生しました")
+finally:
+    print(2)
 # %%
